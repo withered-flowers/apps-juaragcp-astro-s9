eb116921045bd56ce7249103132df8eabe2250e6
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-06-03 21:09:20 +0700

    fix: change event time end

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,2 +1,2 @@
 export const eventTimeStart: string = "2023-05-09T00:00:00.000+07:00";
-export const eventTimeEnd: string = "2023-06-04T00:00:00.000+07:00";
+export const eventTimeEnd: string = "2023-06-04T23:59:00.000+07:00";
